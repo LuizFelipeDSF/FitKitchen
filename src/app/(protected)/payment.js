954f269e79739853b6f6ea91a197c91d5e4810be
LuizFelipeDSF <160753466+LuizFelipeDSF@users.noreply.github.com>
2024-11-13 15:47:52 +0000

    [feat]aula13

--- src/app/(protected)/payment.js
+++ src/app/(protected)/payment.js
@@ -1,9 +1,18 @@
 import { router } from "expo-router";
+import { z } from 'zod';
 import { Picker } from '@react-native-picker/picker';
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { Ionicons } from '@expo/vector-icons';
-import { Button, StyleSheet, Text, TextInput, View } from "react-native"
+import { Button, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, View } from "react-native";
+import { useAuth, userAuth } from "../../hooks/Auth/index";
+
+const paymentSchema = z.object({
+  valor_pago: z.number().gt(0),
+  user_cadastro: z.number().int().positive(),
+  data_pagamento: z.date(),
+  observacao: z.string(),
+})
 
 export default function Payment() {
 
@@ -120,47 +129,106 @@ export default function Payment() {
   const [data, setData] = useState(new Date());
   const [viewCalendar, setViewCalendar] = useState(false);
   const [observacao, setObsetvacao] = useState("");
+  const { user } = useAuth();
+
   const handleCalendar = (event, selectedDate) => {
     setViewCalendar(false);
-    setData(selectedDate); 
+    setData(selectedDate);
   }
+  const valueRef = useRef();
+
+  useEffect(() => {
+    valueRef.current?.focus();
+  }, [])
+
+  const handleChangeValor = (value) => {
+    try {
+      const valorLimpo = value.replace(",", "").replace(".", "");
+      const valorConvertido = Number(valorLimpo) / 100;
+
+      if (valorConvertido === 0 || isNaN(valorConvertido)) {
+        setValor("0,00");
+        return;
+      }
+      let valorPtBR = Intl.NumberFormat("pt-BR", {
+        style: "decimal",
+        minimumFractionDigits: 2,
+      }).format(valorConvertido);
+      setValor(valorPtBR);
+    } catch (error) {
+      setValor("0,00")
+    }
+  };
+
+  const convertValue = (value) => {
+    try {
+      let valorLimpo = value.replace(",", "").replace(".", "");
+      let valorConvertido = Number(valorLimpo) / 100;
+      if (valorConvertido === 0 || isNaN(valorConvertido)) {
+        return 0
+      }
+      return valorConvertido
+    } catch (error) {
+      return valorConvertido
+    }
+  };
+
+  const handleSubmit = async () => {
+    const payment = {
+      user_id: id,
+      user_cadastro: Number(user.user.id),
+      valor_pago: convertValue(valor) > 0 ? convertValue(valor) : 0.01,
+      data_pagamento: data,
+      observacao,
+    };
+
+    try {
+      const result = await paymentSchema.parseAsync(payment);
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
 
   return (
-    <View style={styles.content}>
-      <Text style={{fontSize: 30, paddingBottom: 5}}>Inserir Pagamentos</Text>
-      <View style={styles.inputView}>
-        <Ionicons name="wallet-sharp" size={40} color={'#202020'} />
-        <TextInput placeholder="Valor" keyboardType="decimal-pad" style={styles.inputValor} value={valor} onChange={setValor} />
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <View style={styles.content}>
+        <Text style={{ fontSize: 30, paddingBottom: 5 }}>Inserir Pagamentos</Text>
+        <View style={styles.inputView}>
+          <Ionicons name="wallet-sharp" size={40} color={'#202020'} />
+          <TextInput placeholder="Valor" keyboardType="decimal-pad" style={styles.inputValor} value={valor} onChangeText={(newValue) => handleChangeValor(newValue)} ref={valueRef} />
+        </View>
+        <View style={styles.inputView}>
+          <Picker selectedValue={id} onValueChange={(itemValue, index) => { setId(itemValue) }} style={{ width: "100%" }}>
+            {sugestoes?.map((item) => {
+              return <Picker.Item key={item.id} label={item.nome} value={item.id} />
+            })}
+          </Picker>
+        </View>
+        <View style={styles.inputView}>
+          <Text onPress={() => setViewCalendar(true)} style={styles.inputData}>
+            {data.toLocaleDateString("pt-BR")}
+          </Text>
+          {viewCalendar && (
+            <DateTimePicker
+              value={data}
+              onChange={handleCalendar}
+              mode="date"
+              testID="dateTimePicker"
+            />
+          )}
+        </View>
+        <View style={styles.inputView}>
+          <TextInput placeholder="Observações" style={styles.inputObservacao} value={observacao} onChangeText={setObsetvacao} multiline={true} />
+        </View>
+        <View style={styles.contentButtons}>
+          <Button title="Salvar" onPress={handleSubmit} />
+          <Button title="Continuar" />
+          <Button title="Cancelar" onPress={() => router.back()} />
+        </View>
       </View>
-      <View style={styles.inputView}>
-        <Picker selectedValue={id} onValueChange={(itemValue, index) => { setId(itemValue) }} style={{ width: "100%" }}>
-          {sugestoes?.map((item) => {
-            return <Picker.Item key={item.id} label={item.nome} value={item.id} />
-          })}
-        </Picker>
-      </View>
-      <View style={styles.inputView}>
-        <Text onPress={() => setViewCalendar(true)} style={styles.inputData}>
-          {data.toLocaleDateString("pt-BR")}
-        </Text>
-        {viewCalendar && (
-          <DateTimePicker
-            value={data}
-            onChange={handleCalendar}
-            mode="date"
-            testID="dateTimePicker"
-          />
-        )}
-      </View>
-      <View style={styles.inputView}>
-        <TextInput placeholder="Observações" style={styles.inputObservacao} value={observacao} onChangeText={setObsetvacao} multiline={true} />
-      </View>
-      <View style={styles.contentButtons}>
-        <Button title="Salvar" />
-        <Button title="Continuar" />
-        <Button title="Cancelar" onPress={() => router.back()} />
-      </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -199,8 +267,8 @@ const styles = StyleSheet.create({
     fontSize: 24,
     padding: 10,
   },
-  inputObservacao:{
-    fontFamily:"regular",
+  inputObservacao: {
+    fontFamily: "regular",
     fontSize: 16,
     flex: 1,
   },
